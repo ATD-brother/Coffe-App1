f80caf424e64d94caff1492d7adae0450e008eef
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-01-08 20:04:46 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from "./controls/navigation/navigation.component";
-import { MaterialModule } from './controls/material-ang/material.modules';
+import { NavigationComponent } from './controls/navigation/navigation.component';
+import { MaterialModule } from './material/material.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        NavigationComponent,
-        MaterialModule
-    ]
+  declarations: [AppComponent,],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    NavigationComponent,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatSlideToggleModule,
+   
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/controls/navigation/navigation.component.spec.ts
+++ src/app/controls/navigation/navigation.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MaterialModule } from '../material-ang/material.modules';
+import { MaterialModule } from '../../material/material.module';
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -9,10 +9,9 @@ describe('NavigationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavigationComponent],
-      declarations: [ MaterialModule ],
-    })
-    .compileComponents();
-    
+      declarations: [MaterialModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
