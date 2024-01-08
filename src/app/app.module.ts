b0fbb543df58309f74e22f13f50fd092597acb6b
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-01-08 20:27:30 +0200

    remake app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './controls/navigation/navigation.component';
-import { MaterialModule } from './material/material.module';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
-
+import { NavigationComponent } from "./controls/navigation/navigation.component";
+import { MaterialModule } from './controls/material-ang/material.modules';
 
 @NgModule({
-  declarations: [AppComponent,],
-  providers: [],
-  bootstrap: [AppComponent],
-  imports: [
-    NavigationComponent,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    MatSlideToggleModule,
-   
-  ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NavigationComponent,
+        MaterialModule
+    ]
 })
-export class AppModule {}
+export class AppModule { }
