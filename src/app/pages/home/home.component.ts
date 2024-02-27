3c4abfb78e2f921166ab9c7d0ab2018c5c283969
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-02-27 22:09:41 +0200

    wip

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,8 +4,44 @@ import { MaterialModule } from 'src/app/controls/material-ang/material.modules';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  cards = [
+    {
+      id: 1,
+      image: 'assets/images/HomePage/Coffee.png',
+      name: 'Cappacino',
+      description: '',
+      pirce: '',
+    },
+    {
+      id: 2,
+      image: 'assets/images/HomePage/mocha.png',
+      name: 'Mocha',
+      description: '',
+      pirce: '',
+    },
+    {
+      id: 3,
+      image: 'assets/images/HomePage/late.png',
+      name: 'Latte',
+      description: '',
+      pirce: '',
+    },
+    {
+      id: 4,
+      image: 'assets/images/HomePage/cold-coffe.png',
+      name: 'Cold Java',
+      description: '',
+      pirce: '',
+    },
+    {
+      id: 5,
+      image: 'assets/images/HomePage/Coffee.png',
+      name: 'Cold Java',
+      description: '',
+      pirce: '',
+    },
+  ];
 }
