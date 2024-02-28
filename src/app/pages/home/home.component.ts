149e1faf35cb94f4b727ac50ae0cc23e3315c5a5
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-02-28 18:55:41 +0200

    added carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,36 +12,53 @@ export class HomeComponent {
       id: 1,
       image: 'assets/images/HomePage/Coffee.png',
       name: 'Cappacino',
-      description: '',
-      pirce: '',
+      description: 'lorem inoihasdogi odshgoui hsuioghus ah u hgoiusadfhgasiufhg aisdhsoh',
+      price: '123',
     },
     {
       id: 2,
       image: 'assets/images/HomePage/mocha.png',
       name: 'Mocha',
-      description: '',
-      pirce: '',
+      description: ' oihf adsouadhgfoiu sagduifgias dgfsdgfipugsdaf uisd gfkusadgf f fsdgfiups ',
+      price: '123',
     },
     {
       id: 3,
       image: 'assets/images/HomePage/late.png',
       name: 'Latte',
-      description: '',
-      pirce: '',
+      description: 'vxzc hifkuhgsda sdfgiu fdikusfdf hfoiush vhosud vh sdu vhhsdbvop svfudb',
+      price: '123',
     },
     {
       id: 4,
       image: 'assets/images/HomePage/cold-coffe.png',
       name: 'Cold Java',
-      description: '',
-      pirce: '',
+      description: 'saefg egew rgefg dfg d',
+      price: '123',
     },
     {
       id: 5,
       image: 'assets/images/HomePage/Coffee.png',
       name: 'Cold Java',
-      description: '',
-      pirce: '',
+      description: ' sdfg dfsgsdfg sdfg sdfh dfsg',
+      price: '123',
     },
   ];
+
+  startIndex = 0;
+  endIndex = 2;
+
+  prevCard(): void {
+    if (this.startIndex > 0) {
+      this.startIndex--;
+      this.endIndex--;
+    }
+  }
+
+  nextCard(): void {
+    if (this.endIndex < this.cards.length - 1) {
+      this.startIndex++;
+      this.endIndex++;
+    }
+  }
 }
