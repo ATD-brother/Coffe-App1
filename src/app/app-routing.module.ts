a67c8aeed88641c0a67f9058dcbf5f46a745e8d6
Atudoroae Rafael <rafael_daniel9894@yahoo.com>
2024-03-01 19:30:34 +0200

    create products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { NavigationComponent } from './controls/navigation/navigation.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'menu', component: MenuComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -21,7 +21,7 @@ const routes: Routes = [
 export class AppRoutingModule {
   static Pages = [
     HomeComponent,
-    AboutComponent,
+    ProductsComponent,
     ContactComponent,
     MenuComponent,
     NavigationComponent,
