f57c2dc369194c4287bfa1d8a04db2c8a2fd8f64
Atudoroae Beniamin <111377793+ATDBeni@users.noreply.github.com>
2024-03-02 22:32:07 +0200

    adaugare mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './controls/material-ang/material.modules';
-import { AgmCoreModule } from '@agm/core';
-
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from 'src/app/controls/material-ang/material.modules';
-
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Icon, Style } from 'ol/style';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    const aradCoord = fromLonLat([21.299164883859493, 46.1968485973083]);
+
+    const marker = new Feature({
+      geometry: new Point(aradCoord),
+    });
+
+    const markerStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+      }),
+    });
+
+    marker.setStyle(markerStyle);
+
+    const vectorSource = new VectorSource({
+      features: [marker],
+    });
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        vectorLayer,
+      ],
+      view: new View({
+        center: aradCoord,
+        zoom: 10,
+      }),
+    });
+  }
+
+
   cards = [
     {
       id: 1,
       image: 'assets/images/HomePage/Coffee.png',
       name: 'Cappacino',
-      description: 'lorem inoihasdogi odshgoui hsuioghus ah u hgoiusadfhgasiufhg aisdhsoh',
+      description:
+        'lorem inoihasdogi odshgoui hsuioghus ah u hgoiusadfhgasiufhg aisdhsoh',
       price: '$23',
     },
     {
       id: 2,
       image: 'assets/images/HomePage/mocha.png',
       name: 'Mocha',
-      description: ' oihf adsouadhgfoiu sagduifgias dgfsdgfipugsdaf uisd gfkusadgf f fsdgfiups ',
+      description:
+        ' oihf adsouadhgfoiu sagduifgias dgfsdgfipugsdaf uisd gfkusadgf f fsdgfiups ',
       price: '$12',
     },
     {
       id: 3,
       image: 'assets/images/HomePage/late.png',
       name: 'Latte',
-      description: 'vxzc hifkuhgsda sdfgiu fdikusfdf hfoiush vhosud vh sdu vhhsdbvop svfudb',
+      description:
+        'vxzc hifkuhgsda sdfgiu fdikusfdf hfoiush vhosud vh sdu vhhsdbvop svfudb',
       price: '$33',
     },
     {
@@ -64,22 +120,25 @@ export class HomeComponent {
 
   clients = [
     {
-      personImg:'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
-      Title:"As good as advertised",
-      content:"Lorem ipsum dolor sot a,emt comsecterir adsipic eliter",
-      name:'Alexandru Mihai',
+      personImg:
+        'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
+      Title: 'As good as advertised',
+      content: 'Lorem ipsum dolor sot a,emt comsecterir adsipic eliter',
+      name: 'Alexandru Mihai',
     },
     {
-      personImg:'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
-      Title:"As good as advertised",
-      content:"Lorem ipsum dolor sot a,emt comsecterir adsipic eliter",
-      name:'Alexandru Mihai',
+      personImg:
+        'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
+      Title: 'As good as advertised',
+      content: 'Lorem ipsum dolor sot a,emt comsecterir adsipic eliter',
+      name: 'Alexandru Mihai',
     },
     {
-      personImg:'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
-      Title:"As good as advertised",
-      content:"Lorem ipsum dolor sot a,emt comsecterir adsipic eliter",
-      name:'Alexandru Mihai',
+      personImg:
+        'https://ca.slack-edge.com/TCZPF5WAZ-U05MNLVBJ2F-09e8f1e07877-72',
+      Title: 'As good as advertised',
+      content: 'Lorem ipsum dolor sot a,emt comsecterir adsipic eliter',
+      name: 'Alexandru Mihai',
     },
-  ]
+  ];
 }
